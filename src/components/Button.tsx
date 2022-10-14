ccff365c43707cfff1111a400dbb1230a38f35fd
Matheus Araújo <souzamatheus8174@gmail.com>
2022-10-13 22:54:26 -0300

    refactor: add suport for class overriden

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,21 @@
-import { PropsWithChildren } from 'react';
+import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import clsx from 'clsx';
 
 export type ButtonProps = PropsWithChildren<{
   asChild?: boolean
-}>
+}> & ButtonHTMLAttributes<HTMLButtonElement>
 
-const Button: React.FC <ButtonProps> = ({ children, asChild }) => {
+const Button: React.FC <ButtonProps> = ({ children, asChild, className, ...rest }) => {
   const Comp = asChild ? Slot : 'button'
 
   return (
     <Comp
+      {...rest}
+
       className={clsx(
-        "py-4 px-3 bg-cyan-500 rounded font-semibold transition-colors text-black text-sm w-full hover:bg-cyan-300 focus:ring-2 ring-white",
+        "py-3 px-4 bg-cyan-500 rounded font-semibold transition-colors text-black text-sm w-full hover:bg-cyan-300 focus:ring-2 ring-white",
+        className
       )}
     >
       { children }
