55e0c1b1d741d02f88d2817a4a8fbeb47af4038f
Matheus Araújo <souzamatheus8174@gmail.com>
2022-10-13 22:53:38 -0300

    refactor: add suport for receive more props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,76 @@
-import './styles/global.css'
+import { Envelope, Lock } from 'phosphor-react';
+
+import Button from './components/Button';
+import Checkbox from './components/Checkbox';
+import Heading from './components/Heading';
+import Logo from './components/Logo';
+import Text from './components/Text';
+import { TextInput } from './components/TextInput';
+
+import './styles/global.css';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="w-screen h-full bg-gray-900 flex items-center justify-center text-gray-100 flex-col">
+      <header className="flex flex-col items-center">
+        <Logo />
+
+        <Heading size="lg" className="mt-4">
+          Ignite Lab
+        </Heading>
+
+        <Text size="lg" className="text-gray-400 mt-1">
+          Faça login e comece a usar!
+        </Text>
+      </header>
+
+      <form className="flex gap-4 flex-col items-stretch w-full max-w-sm mt-10">
+        <label htmlFor="email" className="flex flex-col gap-3">
+          <Text className="font-semibold">
+            Endereço de e-mail
+          </Text>
+          <TextInput.Root>
+            <TextInput.icon>
+              <Envelope />
+            </TextInput.icon>
+
+            <TextInput.Input id="email" placeholder="Digite seu e-mail" />
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="password" className="flex flex-col gap-3">
+          <Text className="font-semibold">Sua senha</Text>
+          <TextInput.Root>
+            <TextInput.icon>
+              <Lock />
+            </TextInput.icon>
+
+            <TextInput.Input id="password" placeholder="********" />
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="remember" className="flex items-center gap-2">
+          <Checkbox id="remember" />
+          <Text size="sm" className="text-gray-200">Lembrar de mim por 30 dias</Text>
+        </label>
+
+        <Button type="submit" className="mt-4">Entrar na plataforma</Button>
+      </form>
+
+      <footer className="flex flex-col items-center gap-4 mt-8">
+        <Text asChild={true} size="sm">
+          <a href="#" className="text-gray-400 underline hover:text-gray-200">
+            Esqueceu sua senha?
+          </a>
+        </Text>
+
+        <Text asChild={true} size="sm">
+          <a href="#" className="text-gray-400 underline hover:text-gray-200">
+          Não possui conta? crie uma agora!
+          </a>
+        </Text>
+      </footer>
+    </div>
   )
 }
 
--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,12 +1,13 @@
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
 
-export type CheckboxProps = {}
+export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps {}
 
-const Checkbox: React.FC <CheckboxProps> = ({}) => {
+const Checkbox: React.FC <CheckboxProps> = (props) => {
   return (
     <CheckboxPrimitive.Root
       className="w-6 h-6 p-[2px] bg-gray-800 rounded"
+      {...props}
     >
       <CheckboxPrimitive.Indicator asChild>
         <Check weight="bold" className="h-5 w-5 text-cyan-500" />
